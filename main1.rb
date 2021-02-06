76bbfc6d8108be0a3367b387d320ad6cbcee461c
Yuliya Demeshchik <yuliyademeshchick@gmail.com>
2021-02-06 16:36:44 +0300

    task 2.1 was added

---
+++ main1.rb
@@ -0,0 +1,22 @@
+
+require 'date'
+
+puts 'inter date:'
+date = Date.parse(gets)
+
+puts 'day,  week, month, year?:'
+measure = gets.chomp
+
+puts 'enter N:'
+n = gets.to_i
+
+measures = {'day' => -> (date) {return date.next_day },
+			'week' => -> (date) {return date + 7},
+			'month' => -> (date) {return date.next_month},
+			'year' => -> (date) {return date.next_year}
+}
+
+n.times do 
+	date = measures[measure].call(date) 
+	puts date
+end
